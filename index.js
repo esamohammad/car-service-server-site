9de9479c4581ae7ed99584d9f0066f38bea21716
mdesabd <esa.ce2020@gmail.com>
2023-02-01 01:38:08 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+console.log(uri)
+
 
 
 app.get('/', (req, res) => {
@@ -21,3 +42,5 @@ app.listen(port, () => {
 //*************************************************** */
 // ok this is the basic setup of node express server.
 //*************************************************** */
+
+
