0fd3eeb5adde0c5ea2fed783ebccb13570b020ec
mdesabd <esa.ce2020@gmail.com>
2023-02-01 04:08:29 +0600

    specific id api set

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -20,21 +20,36 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
- //Crud operation start--
+//Crud operation start--
 
 async function run() {
    try {
-       const serviceCollection = client.db('geniusCar').collection('services');
+      const serviceCollection = client.db('geniusCar').collection('services');
 
-       // database ar services gulo ak sathe paoya ---
-       app.get('/services', async (req, res) => {
-           const query = {}
-           const cursor = serviceCollection.find(query);
-           const services = await cursor.toArray();
-           res.send(services);
-       });
+      // database ar services gulo ak sathe paoya ---
+      app.get('/services', async (req, res) => {
+         const query = {}
+         const cursor = serviceCollection.find(query);
+         const services = await cursor.toArray();
+         res.send(services);
+      });
 
-      
+
+      // specific service get api ---(( ID ))
+
+      app.get('/services/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const service = await serviceCollection.findOne(query);
+         res.send(service);
+      });
+
+
+
+
+
+
+
 
    }
    finally {
