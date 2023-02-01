8e8c8b56c4c93ceed4ecb776d4100287c3ec547f
mdesabd <esa.ce2020@gmail.com>
2023-02-01 08:37:06 +0600

    delete orders api create

--- index.js
+++ index.js
@@ -55,15 +55,36 @@ async function run() {
 
 
 
-
-
-
       // orders api
+      app.get('/orders', async (req, res) => {
+         let query = {};
+
+         if (req.query.email) {
+            query = {
+               email: req.query.email
+            }
+         }
+
+         const cursor = orderCollection.find(query);
+         const orders = await cursor.toArray();
+         res.send(orders);
+      });
+
       app.post('/orders', async (req, res) => {
          const order = req.body;
          const result = await orderCollection.insertOne(order);
          res.send(result);
+      });
+
+      //Delete Api
+      app.delete('/orders/:id', async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const result = await orderCollection.deleteOne(query);
+         res.send(result);
       })
+
+
 
 
 
