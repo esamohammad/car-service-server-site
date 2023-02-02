aaa71fae518fa3bbbb78e755fefb00e39c9b5c51
mdesabd <esa.ce2020@gmail.com>
2023-02-03 02:11:37 +0600

    token saved in lokal storage

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -32,12 +33,11 @@ async function run() {
 
 
 
-      app.post('/jwt', (req, res) => {
+      app.post('/jwt', (req, res) =>{
          const user = req.body;
          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
          res.send({token})
      })  
-
 
 
       // database ar services gulo ak sathe paoya ---
