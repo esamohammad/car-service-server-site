6cd948eee68a0454c01419e7ffddddfca29d4779
mdesabd <esa.ce2020@gmail.com>
2023-02-03 00:14:52 +0600

    jwt token get at console

--- index.js
+++ index.js
@@ -34,8 +34,9 @@ async function run() {
 
       app.post('/jwt', (req, res) => {
          const user = req.body;
-         console.log(user);
-      })
+         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+         res.send({token})
+     })  
 
 
 
