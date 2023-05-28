56ed0f3ed42d1bf6a4e8328e80214bf72960c768
esamohammad <esa.ce2020@gmail.com>
2023-05-28 16:17:08 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 // this is from the Mongodb.
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ppcaliy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ddkgs5g.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
